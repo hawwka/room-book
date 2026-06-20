de77f166d4460fa9a27e646d8bc9ed67d087a02c
hawwka <poiuminaj@gmail.com>
2026-06-20 10:11:00 +0300

    Spring boot project

---
+++ roombook/src/test/java/io/github/hawwka/roombook/RoombookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hawwka.roombook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoombookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
